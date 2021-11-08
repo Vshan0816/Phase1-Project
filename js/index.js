cd417fa3d49d6e00c8aa3af72b44246a14df796f
Vincent <vshan0816@gmail.com>
2021-11-08 12:58:49 -0500

    add event listenersstructure for images

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // Arrays to create to make code less dry
 const characterImgs = [
-    shiki, arcueid, akiha, ciel, hisui, kohaku, miyako, kouma, noel, roa, flov, redArc, saber
+    shiki, arcueid, akiha, ciel, hisui, kohaku, miyako, kouma, noel, roa, vlov, redArc, saber
 ]
 
 // what to include under DOMContentloaded
 document.addEventListener("DOMContentLoaded", handlePageLoaded)
+
 function handlePageLoaded(){
-    characterImgs.forEach(characterImg => {addEventListener("click", handleCharacter)
-})}
-
-function handleCharacter{
-
+    shiki.addEventListener("click", handleShiki)
+    arcueid.addEventListener("click", handleArcueid)
+    akiha.addEventListener("click", handleAkiha)
+    ciel.addEventListener("click", handleCiel)
+    hisui.addEventListener("click", handleHisui)
+    kohaku.addEventListener("click", handleKohaku)
+    miyako.addEventListener("click", handleMiyako)
+    kouma.addEventListener("click", handleKouma)
+    noel.addEventListener("click", handleNoel)
+    roa.addEventListener("click", handleRoa)
+    vlov.addEventListener("click", handleVlov)
+    redArc.addEventListener("click", handleRedArc)
+    saber.addEventListener("click", handleSaber)
 }
-
+function handleShiki(){
+    
+}
 // What I want to do for each character
 // create element CharacterCard
 // append CharacterCard to page
