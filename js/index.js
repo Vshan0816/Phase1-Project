a784f5b616b17b27f9386caa72a494aab5f88fb4
Vincent <vshan0816@gmail.com>
2021-11-28 19:44:19 -0500

    remove JS notes, add catch

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-//global array for fetch data
+
 let characters = []  
 
-//fetch function
 function fetchCharacters(){
     fetch("http://localhost:3000/typeLuminaCharacter")
     .then(resp => resp.json())
@@ -9,19 +8,19 @@ function fetchCharacters(){
         characters = data
         data.forEach(handleCharacter)
     })
+    .catch(function (error){
+        console.log(error.message)
+    })
 }
 
-// what to include under DOMContentloaded
 document.addEventListener("DOMContentLoaded", handlePageLoaded)
 
-// functions under DOMContentloaded
 function handlePageLoaded(){
     fetchCharacters()
     document.querySelector("#form").addEventListener("submit", handleSubmit)
     document.querySelector("#clear").addEventListener("click", handleClear)   
 }
 function handleCharacter(character){
-    // make elements
     const charImg1 = document.createElement("img")
     
     const charDiv = document.createElement("div")
@@ -31,20 +30,19 @@ function handleCharacter(character){
     const charStory = document.createElement("p")
     const table = document.createElement("table")
 
-    // set information to elements
     charImg1.src = character.image1
 
     charName.textContent = character.name
     charImg2.src = character.image2
     charStory.textContent = character.story
     storyTitle.textContent = "Story"
-    table.innerHTML = `
+    table.innerHTML =  `
     <tr>
         <th>MoveName</th>
         <th>Startup</th>
         <th>Frame Advantage on Block</th>
-    </tr>
-    `
+    </tr>`
+    
     character.moveset.forEach(move => {
         const keyName = Object.keys(move)[0]
         table.innerHTML += `
@@ -54,7 +52,7 @@ function handleCharacter(character){
         <td>${move[keyName].onBlock}</td>
     </tr>`
     })
-    // set attributes
+    
     charDiv.id = "charDivID"
     charName.className = "h2"
     charImg2.className = "singleCharacter"
@@ -62,19 +60,13 @@ function handleCharacter(character){
     charStory.id = "charStoryID"
     table.id = "tableID"
     
-    // append character images to page on launch
     document.querySelector("#character-list").appendChild(charImg1)
 
-    // add event listeners to each image
     charImg1.addEventListener("click", handleOneCharacter)
 
-
     function handleOneCharacter(){
-    // append to charDiv
     charDiv.append(charName,charImg2,storyTitle,charStory, table)
-    // clear #single-character
     document.querySelector("#single-character").innerHTML=""
-    // append to page
     document.querySelector("#single-character").appendChild(charDiv)
     }
 }
@@ -100,37 +92,4 @@ function handleSubmit(e){
 function handleClear(){
     document.querySelector("#paragraph").innerHTML = ""
 }
-
-// What I want to do for each character
-// create div for each Character
-// append CharacterCard to page
-    // what do i append to
-        // #character-list
-
-// What does CharacterCard contain?
-    // name, story, movelist with frame data
-// How do i use Fetch to GET what i need
-    // Fire fetch on DOMContentLoaded
-    // use data with forEach, to render characters on page
-    // render images on DOMContentLoaded, through function
-    // DOMContentLoaded -> handlePageLoaded -> fetchCharacters -> handleCharacter
-
-// How do i create a table  
-    // use innerHTML
-    // for each move we should create a row in the table
-
-// what does a find function do
-    // The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
-    // If no values satisfy the testing function, undefined is returned.
-// Do i need a find function? If so why?
-// How do i create a find function? 
-// what do i need to return?
-    // character move name, frame data
-// what i need to be in the input
-    // Movedata
-// Thoughts
-    // Do i need to iterate? Yes in order to find each movename
-    // If input.value = movename, append movename, startup and onblock
-
-// why an array of objects, versus, an object with seperate keys. 
 
